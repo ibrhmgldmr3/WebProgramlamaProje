cbfc5a5ca7ce266a80f57409f41b8c87060492a5
Suleyman Samet Kaya <suleymansametkaya@gmail.com>
2024-12-14 17:06:02 +0300

    Navbar ve Login Page Düzenlemeleri Yapıldı

--- WebProgProje/obj/Debug/net8.0/WebProgProje.AssemblyInfo.cs
+++ WebProgProje/obj/Debug/net8.0/WebProgProje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProgProje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4e68a5b0a358891dbec5466aee6f1cac0bc52d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c443cd9551b3214d23f5e66862862ba3bfa5b594")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProgProje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProgProje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
