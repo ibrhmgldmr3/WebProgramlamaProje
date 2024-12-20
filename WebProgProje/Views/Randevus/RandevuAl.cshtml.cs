da7dc8c19aba6f68f98c0765c02a8912b2b4ada3
İbrahim Güldemir <ibrahim.guldemir@ogr.sakarya.edu.tr>
2024-12-20 19:06:00 +0300

    çalışan ve randevu controllerları düzenleme

---
+++ WebProgProje/Views/Randevus/RandevuAl.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebProgProje.Views.Randevus
+{
+    public class RandevuAlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
