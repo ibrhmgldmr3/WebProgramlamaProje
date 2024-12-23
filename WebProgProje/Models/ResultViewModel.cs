c19893e61589ca8a4355bcb181f96842e4164af8
Suleyman Samet Kaya <suleymansametkaya@gmail.com>
2024-12-23 19:02:21 +0300

    G

---
+++ WebProgProje/Models/ResultViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebProgProje.Models
+{
+    public class ResultViewModel
+    {
+        public string? ResultImage { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
